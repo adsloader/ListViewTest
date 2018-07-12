5b89a041bf56b7b0a12b6262fb50c9f1524ffe2b
VintageAppMaker <31234716+VintageAppMaker@users.noreply.github.com>
2018-07-12 10:04:14 +0900

    Update ListData.java

--- src/com/campandroid/listviewtest/ListData.java
+++ src/com/campandroid/listviewtest/ListData.java
@@ -3,13 +3,10 @@ package com.campandroid.listviewtest;
 import android.graphics.drawable.Drawable;
 
 public class ListData {
-    // 아이콘
     public Drawable mImage;
     
-    // 제목
     public String mTitle;
     
-    // 날짜
     public String mDetail;
     
 }
